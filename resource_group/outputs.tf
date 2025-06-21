d6ed5524fa2cc3fab5445587cf2a38e01a6e2e3a
AnushaV05 <av.anushavarghese@outlook.com>
2025-06-21 16:50:44 -0600

    Added main.tf files to all module folders

--- resource_group/outputs.tf
+++ resource_group/outputs.tf
@@ -1,10 +1,11 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.main.name
-}
-output "id" {
-  value = azurerm_resource_group.rg.id
-}
-output "location" {
-  value = azurerm_resource_group.rg.location
+  value = azurerm_resource_group.resource_group.name
 }
 
+output "id" {
+  value = azurerm_resource_group.resource_group.id
+}
+
+output "location" {
+  value = azurerm_resource_group.resource_group.location
+}
